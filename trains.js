2c1194b00b0d5487ce7406be6972972ed413866c
henrikasvand <henri.kasvand@gmail.com>
2020-02-04 14:02:59 +0200

    First Fetch from HKI to tampere from traindata

--- trains.js
+++ trains.js
@@ -2,13 +2,14 @@
 //document.getElementById("searchdepart").addEventListener("click",)
 
 function myFunction(){
-    fetch('https://rata.digitraffic.fi/api/v1/live-trains/station/HKI/TPE')
+    fetch(`https://rata.digitraffic.fi/api/v1/live-trains/station/HKI/TPE`)
     .then(res => res.json())
     .then(data => {
         console.dir(data);
         let tempstring = "";
         for (let d of data) {
-            tempstring += `<div>${d}</div>`;
+            tempstring += `<div>${d.trainNumber}</div>`;
+            console.dir(d);
         }
         trainSchedule.innerHTML=tempstring;
     }).catch((error)=>{
