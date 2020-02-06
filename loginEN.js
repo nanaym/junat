ec62a5b05424e6d19666fe2792bc944e4317996a
OskariAuvinen <55886851+OskariAuvinen@users.noreply.github.com>
2020-02-06 14:18:06 +0200

    kielijavascriptit

---
+++ loginEN.js
@@ -0,0 +1,40 @@
+// Nimi ja salasana rekisteröidään
+var newName = document.getElementById('username');
+var newPw = document.getElementById('pw');
+
+// Tallentaa käyttäjätunnuksen ja salasanan
+function store() {
+    localStorage.setItem(newName.value, newPw.value);
+    console.log(newName.value, newPw.value);
+    alert(`Username ${newName.value} succesfully created. You can now log in.`)
+}
+
+// check if stored data from register-form is equal to entered data in the   login-form
+function check() {
+    var newName = document.getElementById('userName');
+    var newPw = document.getElementById('userPw');
+    console.log(newName.value + " ! " + newPw.value)
+
+    // Tallennettu data rekiströnti-lomakkeesta
+    let storedPw = localStorage.getItem(newName.value);
+    let localData = (newName.value + storedPw);
+
+    // Syötetty login data
+    let userName = document.getElementById('userName');
+    let inputData = (userName.value + newPw.value);
+    console.log(localData + " :) " + inputData);
+
+    // tarkistaa matchaako rekisteröinti ja kirjautuminen
+    if (localData == inputData) {
+        alert(`Welcome ${userName.value}!`);
+        localStorage.setItem('currentUser', userName.value);
+        activeUser()
+        // currentUsername.innerHTML = `<p> Sisäänkirjautunut: ${currentUser}</p>`
+    } else {
+        alert('Wrong username and or password!');
+    }
+}
+function activeUser() {
+    let currentUser = localStorage.getItem('currentUser', userName.value);
+currentUsername.innerHTML = `<p> Logged in: ${currentUser}</p>`
+}
---
+++ loginSWE.js
@@ -0,0 +1,40 @@
+// Nimi ja salasana rekisteröidään
+var newName = document.getElementById('username');
+var newPw = document.getElementById('pw');
+
+// Tallentaa käyttäjätunnuksen ja salasanan
+function store() {
+    localStorage.setItem(newName.value, newPw.value);
+    console.log(newName.value, newPw.value);
+    alert(`Användarnamn ${newName.value} skapad. Ni kan nu logga in.`)
+}
+
+// check if stored data from register-form is equal to entered data in the   login-form
+function check() {
+    var newName = document.getElementById('userName');
+    var newPw = document.getElementById('userPw');
+    console.log(newName.value + " ! " + newPw.value)
+
+    // Tallennettu data rekiströnti-lomakkeesta
+    let storedPw = localStorage.getItem(newName.value);
+    let localData = (newName.value + storedPw);
+
+    // Syötetty login data
+    let userName = document.getElementById('userName');
+    let inputData = (userName.value + newPw.value);
+    console.log(localData + " :) " + inputData);
+
+    // tarkistaa matchaako rekisteröinti ja kirjautuminen
+    if (localData == inputData) {
+        alert(`Välkommen ${userName.value}!`);
+        localStorage.setItem('currentUser', userName.value);
+        activeUser()
+        // currentUsername.innerHTML = `<p> Sisäänkirjautunut: ${currentUser}</p>`
+    } else {
+        alert('Fel username eller lösenord');
+    }
+}
+function activeUser() {
+    let currentUser = localStorage.getItem('currentUser', userName.value);
+currentUsername.innerHTML = `<p> Inloggad: ${currentUser}</p>`
+}
